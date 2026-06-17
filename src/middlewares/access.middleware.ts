adc47f7c2acc7513087238589b4d248a7777b81f
Eduardo Gonçalves Libindo <eduardogoncalves@pirahy.local>
2026-06-17 08:46:15 -0300

    removendo a logica middleware access

--- src/middlewares/access.middleware.ts
+++ src/middlewares/access.middleware.ts
@@ -11,25 +11,25 @@ export async function accessMiddleware(
   res: Response,
   next: NextFunction
 ) {
-  const password = String(req.headers.password);
+  // const password = String(req.headers.password);
 
-  if (!password) {
-    return res.status(401).json({
-      message: "Password da API não informado"
-    });
-  }
+  // if (!password) {
+  //   return res.status(401).json({
+  //     message: "Password da API não informado"
+  //   });
+  // }
   
   try {
 
-    const access = await bcrypt.compare(password, SECRET_HASH_KEY_API);
+  //   const access = await bcrypt.compare(password, SECRET_HASH_KEY_API);
 
-    if (!access) {
-        return res.status(401).json({
-        message: "Senha de acesso da API Incorreta",
-        });
-    }
+  //   if (!access) {
+  //       return res.status(401).json({
+  //       message: "Senha de acesso da API Incorreta",
+  //       });
+  //   }
 
-    console.log("API Autenticada");
+  //   console.log("API Autenticada");
 
     next();
   } catch {
