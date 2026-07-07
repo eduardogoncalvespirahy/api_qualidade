0b4867466e0bfa9cc483202f37b9ba6749d179d0
Eduardo Gonçalves Libindo <eduardogoncalves@pirahy.local>
2026-07-07 13:59:04 -0300

    arrumando umas rotas

--- src/routes/form.routes.ts
+++ src/routes/form.routes.ts
@@ -10,6 +10,6 @@ router.post("/", authMiddleware, roleMiddleware(["ADMIN", "LIDER"]), controller.
 router.get("/", authMiddleware, roleMiddleware(["ADMIN", "LIDER", "INSPETOR"]), controller.findAll);
 router.get("/:id", authMiddleware, roleMiddleware(["ADMIN", "LIDER", "INSPETOR"]), controller.findById);
 router.put("/:id", authMiddleware, roleMiddleware(["ADMIN", "LIDER"]), controller.update);
-router.delete("/:id", authMiddleware, roleMiddleware(["ADMIN","LIDER"]), controller.delete);
+router.delete("/:id", authMiddleware, roleMiddleware(["ADMIN", "LIDER"]), controller.delete);
 
 export default router;
