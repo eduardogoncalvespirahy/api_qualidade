b2549c20abb52c4034483f8ab05be79efbfb33eb
Eduardo Gonçalves Libindo <eduardogoncalves@pirahy.local>
2026-07-15 16:42:00 -0300

    erro ao criar RepairerAnswerResult - resolvido

--- src/routes/repairerAnswerResult.routes.ts
+++ src/routes/repairerAnswerResult.routes.ts
@@ -6,12 +6,12 @@ import { roleMiddleware } from "../middlewares/role.middleware";
 const router = Router();
 const controller = new RepairerAnswerResultController();
 
-router.post("/", authMiddleware, roleMiddleware(["ADMIN", "LIDER"]), controller.create);
+router.post("/", authMiddleware, roleMiddleware(["ADMIN", "LIDER", "INSPETOR"]), controller.create);
 router.get("/", authMiddleware, roleMiddleware(["ADMIN", "LIDER", "INSPETOR"]), controller.findAll);
 router.get("/:answerResultId/:userId", authMiddleware, roleMiddleware(["ADMIN", "LIDER", "INSPETOR"]), controller.findById);
 router.get("/answerResultId/:answerResultId", authMiddleware, roleMiddleware(["ADMIN", "LIDER", "INSPETOR"]), controller.findByAnswerResultId);
 router.get("/userId/:userId", authMiddleware, roleMiddleware(["ADMIN", "LIDER", "INSPETOR"]), controller.findByUserId);
-router.put("/:answerResultId/:userId", authMiddleware, roleMiddleware(["ADMIN", "LIDER"]), controller.update);
+router.put("/:answerResultId/:userId", authMiddleware, roleMiddleware(["ADMIN", "LIDER", "INSPETOR"]), controller.update);
 router.delete("/:answerResultId/:userId", authMiddleware, roleMiddleware(["ADMIN", "LIDER"]), controller.delete);
 
 export default router;
